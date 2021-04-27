d8ce40f5dc36e2ae48768ddd25b50b4633c24074
Logan D.G. Smith <loganda.smith@ufl.edu>
2021-04-27 19:52:35 -0400

    Initial tests completed, working on real world documents

--- cado.py
+++ cado.py
@@ -38,7 +38,7 @@ class TextClassifier:
         for index, word_sets in enumerate(self.subject_words):
             new_set = []
             for word in word_sets:
-                stripped_word = word.strip('\n')
+                stripped_word = word.strip('\n').lower()
                 new_set.append((stripped_word, self.subjects[index]))
                 self.subject_dictionary[stripped_word] = index
             self.subject_words[index] = new_set
@@ -50,32 +50,46 @@ class TextClassifier:
 
         self.classifier = nltk.NaiveBayesClassifier.train(feature_sets)
 
-    def predict(self):
-        pass
+    def predict(self, lines: list) -> str:
+        if self.classifier == None:
+            print('No classifier is initialized! You must call train() before predict()!')
+            return
+
+        return self.classifier.classify(self.matching_features(lines))
 
     # Naive classification of text based on the similarity to subject
     def matching_features(self, line: str):
         scores = [0] * len(self.subjects)
 
-        # Find all words in the top occuring
-        words = nltk.word_tokenize(line)
+        if(type(line) == list):
+            for word in line:
+                subject_index = self.subject_dictionary.get(word, -1)
+                if subject_index == -1:
+                    continue
+                scores[subject_index] += 1
+        else:
+            subject_index = self.subject_dictionary.get(line, -1)
+            if subject_index > -1:
+                scores[subject_index] += 1
 
-        for word in words:
-            subject_index = self.subject_dictionary.get(word, -1)
-            if subject_index == -1:
-                continue
-            scores[subject_index] += 1
-
-        most_like = scores.index(max(scores))
-
+        most_like = 0
+        highest_score = -1
+        for index, score in enumerate(scores):
+            if score > highest_score:
+                highest_score = score
+                most_like = index
+        
         # Returning the scores
-        return {'subject' : most_like}
+        return {'subject' : self.subjects[most_like]}
 
 
 def read_docx(path: str) -> list:
     """Reads a docx and returns list of non-symbol words"""
-    doc = docx.Document(path)
-    lines = [paragraph.text for paragraph in doc.paragraphs]
+    try:
+        doc = docx.Document(path)
+        lines = [paragraph.text for paragraph in doc.paragraphs]
+    except:
+        lines = ''
     
     # Read lines of the word doc
     words = []
@@ -83,39 +97,45 @@ def read_docx(path: str) -> list:
         if line != '':
             words += nltk.word_tokenize(line)
 
-    # Strip symbols and put words in lowercase
-    tokens = []
-    for word in words:
-        if word not in '!@#$%^&*(),<>./?\'\"\\':
-            tokens.append(word.lower())
-
-    return tokens
+    return strip_symbols(words)
 
 def read_pdf(path: str) -> list:
     """Reads a pdf and returns list of non-symbol words"""
-    # Open the PDF
-    with open(path, 'rb') as pdf:
-        # Read the contents of the PDF
-        pdf = PyPDF2.PdfFileReader(pdf)
+    try:
+        # Open the PDF
+        with open(path, 'rb') as pdf:
+            # Read the contents of the PDF
+            pdf = PyPDF2.PdfFileReader(pdf)
+            pages = []
+            for x in range(pdf.numPages):
+                page = pdf.getPage(x).extractText().replace('\n', '')
+                pages += nltk.word_tokenize(page)
+    except:
         pages = []
-        for x in range(pdf.numPages):
-            page = pdf.getPage(x).extractText().replace('\n', '')
-            pages += nltk.word_tokenize(page)
 
-        # Strip symbols and put words in lowercase
-        tokens = []
-        for word in pages:
-            if word not in '!@#$%^&*(),<>./\'\"\\':
-                tokens.append(word.lower())
+    return strip_symbols(pages)
 
+def read_txt(path: str) -> list:
+    """Read text files"""
+    textstr = ''
+    try:
+        with open(path, 'r') as txt:
+            textstr = txt.read()
+    except:
+        textstr = ''
+    
+    return strip_symbols(nltk.word_tokenize(textstr))
+
+def strip_symbols(text: list) -> list:
+    """Strip symbols from a list of words and put words in lowercase"""
+    tokens = []
+    for word in text:
+        if word not in '!@#$%^&*(),<>./\'\"\\':
+            tokens.append(word.lower())
     return tokens
-
-def file_content_sort(downloads_path):
-    """Sorts files into subfolders based on their content"""
 
 def create_test_files(test_path):
     """Creates a number of empty files for testing the file extension sorting"""
-    
     # Quick filetype declaration
     print('Creating test files...')
     file_types = {
@@ -146,6 +166,70 @@ def create_test_files(test_path):
     for key in file_types.keys():
         filename = key[1:] + '_test' + key
         Path(os.path.join(test_path, filename)).touch()
+
+def classify_files(path: str) -> list:
+    """Classifies files and sorts them into subfolders"""
+    # Initializations
+    classifier = TextClassifier()
+    classifier.train()
+    classifications = set() # Folders to create
+    file_classifications = [] # Tuples of (filename, classification) 
+
+    # Attempt to collect files
+    files = [file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))]
+    
+    # Attempt to read files
+    for file in files:
+        text = []
+        classification = 'Misc'
+        if file.endswith('.docx'):
+            text = read_docx(file)
+        elif file.endswith('.pdf'):
+            text = read_pdf(file)
+        elif file.endswith('.txt'):
+            text = read_txt(file)
+
+        if text:
+            classification = classifier.predict(text)
+        
+        classifications.add(classification)
+        file_classifications.append((file, classification))
+
+    # Create folders if they don't exist
+    for folder in classifications:
+        print(f'  Attempting to Create {os.path.join(path, folder)}...')
+        Path(os.path.join(path, folder)).mkdir(parents=True, exist_ok=True)
+
+    # Sort the files into their folders
+    for file, classification in file_classifications:
+        # Define the source and destination
+        folder_to = os.path.join(path, classification)
+        folder_to = os.path.join(folder_to, file)
+        folder_from = os.path.join(path, file)
+
+        # Debugging output
+        print(f'    FROM: {folder_from}')
+        print(f'      TO: {folder_to}')
+
+        # Rename (move) the file to the new folder
+        os.rename(folder_from, folder_to)
+
+def file_content_sort(downloads_path):
+    """Sorts the files in subdirectories based on content"""
+    docs_path = os.path.join(downloads_path, 'Docs')
+    pdfs_path = os.path.join(downloads_path, 'PDFs')
+
+    # Determine if the paths are valid
+    if os.path.isdir(docs_path):
+        classify_files(docs_path)
+    else:
+        print(f'Cannot find "Docs" Folder.')
+
+    if os.path.isdir(pdfs_path):
+        classify_files(pdfs_path)
+    else:
+        print(f'Cannot find "PDFs" Folder.')
+
 
 def file_ext_sort(downloads_path):
     """Sorts the files in a directory based on file type. Will NOT sort folders."""
@@ -215,6 +299,7 @@ def main():
     parser.add_help = True
     parser.add_argument('-p', '--path', help='Override default behavior. Path to the folder to sort.')
     parser.add_argument('-t', '--testfiles', action='store_true', help='Creates test files instead of sorting. Must specify the "-p" flag to use this.')
+    parser.add_argument('-n', '--nocontent', action='store_true', help='Disables content sorting.')
     parser.add_argument('--version', action='version', version=__version__)
     flags = parser.parse_args()
 
@@ -228,6 +313,11 @@ def main():
         # Sorting by file extension
         print(f'Attempting to sort {flags.path}')
         file_ext_sort(flags.path)
+        if not flags.nocontent:
+            print('Sorting content...')
+            file_content_sort(flags.path)
+        else:
+            print('Skipping content sort.')
         print('Complete!')
         return
 
@@ -257,12 +347,22 @@ def main():
     # Determine if the path is valid
     if os.path.isdir(path):
         file_ext_sort(path)
+
+        is_content_sort = input('Would you like to perform a content sort? (Y/n): ')
+        if not flags.nocontent or (is_content_sort.lower() == 'y' or is_content_sort == ''):
+            print('Sorting content...')
+            file_content_sort(path)
+        else:
+            print('Skipping content sort.')
     else:
         print(f'Cannot find the path: "{path}".')
         
 if __name__ == '__main__':
-    #main()
-    print('UNCOMMENT THE MAIN FUNCTION')
-    tc = TextClassifier()
-    tc.train()
-    print(tc.subject_words)
+    main()
+    # print('UNCOMMENT THE MAIN FUNCTION')
+    # tc = TextClassifier()
+    # tc.train()
+    # #doc = read_txt('expectations.txt')
+    # #doc = read_pdf('hacker.pdf')
+    # doc = read_docx('expectations.docx')
+    # print(tc.predict(doc))
